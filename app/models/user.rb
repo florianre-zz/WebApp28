1221016d78065c2e7558d5ed5d8e708c1544ae11
fre14 <femile@blueyonder.co.uk>
2016-05-24 19:20:06 +0100

    Create script to load university and corresponding extension in hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # validates :email, :format => /\A([\w+\-]\.?)+@ic.ac.uk\z/i
+  validates :email, :format => /\A([\w+\-]\.?)+@ic.ac.uk\z/i
+
+  def create_table
+    university_table = Hash.new
+    Dir.glob('../../universities/*.txt') do |f|
+      file = File.open(f, "r")
+      data = file.read
+      university_table["@#{File.basename(f, ".txt")}.ac.uk"] = data
+      file.close
+    end
+    return university_table
+  end
+
 end
