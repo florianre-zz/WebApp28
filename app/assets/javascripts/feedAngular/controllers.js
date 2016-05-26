40573f9253b3c62f42a6db17e8e94bf857c94976
Satrnin Pugnet <saturnin.pugnet14@ic.ac.uk>
2016-05-26 14:14:08 +0100

    Click on dropdown implemented

--- app/assets/javascripts/feedAngular/controllers.js
+++ app/assets/javascripts/feedAngular/controllers.js
@@ -6,31 +6,33 @@ var feedModule = angular.module('feedModule', []);
 
 feedModule.controller('feedController', ['$scope',
   function($scope) {
-    $scope.test = "12fqffdfdfdfdfds3456465";
     $scope.events = [
       {
         "sport": "tennis",
         "date": "Thurday 12 February 2016, 2pm",
         "needed": "1 person needed",
         "place": "Hyde Park, meet at Ethos in front of Imperial",
-        "id": "Posted by Paul Vidal"
+        "id": "Posted by Paul Vidal",
+        "university":"imperial"
       },
       {
         "sport": "dance",
         "date": "Thurday 6 February 2014, 6am",
         "needed": "100 person needed",
         "place": "Waterloo, meet at UCL",
-        "id": "Posted by Corentin Herbinet"
+        "id": "Posted by Corentin Herbinet",
+        "university":"lse"
       },
       {
         "sport": "football",
         "date": "Monday 3 jauary 2016, 3pm",
         "needed": "2 person needed",
         "place": "Liverpool, meet in Paris",
-        "id": "Posted by Floriant Emile"
+        "id": "Posted by Floriant Emile",
+        "university":"ucl"
       }
       ]
-      $scope.universitySearch = "";
+      $scope.searchUniversity = "";
       $scope.selectedUni = "";
       $scope.universities = [
         {
@@ -60,10 +62,15 @@ feedModule.controller('feedController', ['$scope',
         {
           "name":"ustbsbscl"
         },
-      ]
+      ];
 
-      $scope.getUniversities = function() {
-      if($scope.universitySearch == "") {
+      $scope.filterUniversity = "";
+      $scope.updateFilterUniversity = function() {
+        $scope.filterUniversity = $scope.searchUniversity;
+      }
+
+      $scope.getSearchUniversities = function() {
+      if($scope.searchUniversity == "") {
         return [];
       } else {
         return $scope.universities;
