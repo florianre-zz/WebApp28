d1f9e656706d62e6fcb8b83fa8aebf51e3cd1eef
Paul Vidal <paul.vidal14@imperial.ac.uk>
2016-05-29 19:36:12 +0100

    user login now available on feed page

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,4 +1,20 @@
 class FeedController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => [:index]
+  helper_method :resource_name, :resource, :devise_mapping
+
   def index
+  end
+
+  # Helper methods
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
