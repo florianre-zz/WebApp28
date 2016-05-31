9b283f35b0a6fe7f29af2b5b0119433f24e344fe
fre14 <femile@blueyonder.co.uk>
2016-05-31 18:33:41 +0100

    Fill Sports table when running seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'university/mail_university_hash_generator'
 require 'university/university_country'
+require 'sports/sports_hash_generator'
 
 # This file should contain all the record creation needed to seed the database
 # with its default values.
@@ -12,5 +13,12 @@ if UniversityMail.count == 0
                         UniversityCountry::ENGLAND)
   universities_hash.each do |name, population|
     UniversityMail.create(mail_extension: name, university_name: population)
+  end
+end
+
+if Sport.count == 0
+  sports_hash = SportsHashGenerator.generate_sports_hash()
+  sports_hash.each do |name, _|
+    Sport.create(name: name)
   end
 end
