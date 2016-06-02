d84375dd3a7041c11ee015fb4cd8cc3c15e0d500
fre14 <femile@blueyonder.co.uk>
2016-06-02 11:28:47 +0100

    Refactor events test to use factory_girl

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,119 +3,80 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
 
   describe '#date' do
-    event = Event.new(sport: 'Football',
-                      start_time: '20:00:00', end_time: '21:00:00',
-                      university_location: 'Imperial College London')
-
     it 'should fail past date validation' do
-      event.date = Date.yesterday
-      event.valid? # run validations
+      event = build(:event, date: Date.yesterday)
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:date)
-      expect(event.errors_on(:date)).to include('Date cannot be in the past!')
     end
 
     it 'should pass past date validation' do
-      event.date = Date.tomorrow
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:date)
+      event = build(:event)
+      expect(event).to be_valid
     end
   end
 
   describe '#start_time' do
-    event = Event.new(sport: 'Football',
-                      date: Date.tomorrow,
-                      end_time: '20:00:00',
-                      university_location: 'Imperial College London')
-
     it 'should pass time are logical validation' do
-      event.start_time = '21:00:00'
-      event.valid? # run validations
+      event = build(:event, start_time: '22:00:00')
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:end_time)
-      expect(event.errors_on(:end_time)).to include('End time should be later than the start time!')
     end
 
     it 'should pass time are logical validation' do
-      event.start_time = '18:00:00'
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:end_time)
+      event = build(:event)
+      expect(event).to be_valid
     end
   end
 
   describe '#end_time' do
-    event = Event.new(sport: 'Football',
-                      date: Date.tomorrow,
-                      start_time: '20:00:00',
-                      university_location: 'Imperial College London')
-
     it 'should fail time are logical validation' do
-      event.end_time = '19:00:00'
-      event.valid? # run validations
+      event = build(:event, end_time: '19:00:00')
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:end_time)
-      expect(event.errors_on(:end_time)).to include('End time should be later than the start time!')
     end
 
     it 'should pass time are logical validation' do
-      event.end_time = '21:00:00'
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:end_time)
+      event = build(:event)
+      expect(event).to be_valid
     end
   end
 
   describe '#min_participants' do
-    event = Event.new(sport: 'Football',
-                      date: Date.tomorrow,
-                      start_time: '20:00:00', end_time: '21:00:00',
-                      university_location: 'Imperial College London')
-
     it 'should fail minimum number of participants validation' do
-      event.min_participants = 1
-      event.valid? # run validations
+      event = build(:event, min_participants: 1)
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:min_participants)
-      expect(event.errors_on(:min_participants)).to include('Minimum number of participants should be at least 2!')
     end
 
     it 'should pass university location validation' do
-      event.min_participants = 10
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:min_participants)
+      event = build(:event, min_participants: 10)
+      expect(event).to be_valid
     end
   end
 
   describe '#university_location' do
-    event = Event.new(sport: 'Football',
-                      date: Date.tomorrow,
-                      start_time: '20:00:00', end_time: '21:00:00')
-
     it 'should fail university location validation' do
-      event.university_location = 'Jupiter'
-      event.valid? # run validations
+      event = build(:event, university_location:'Jupiter')
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:university_location)
-      expect(event.errors_on(:university_location)).to include('University location is invalid.')
     end
 
     it 'should pass university location validation' do
-      event.university_location = 'Imperial College London'
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:sport)
+      event = build(:event)
+      expect(event).to be_valid
     end
   end
 
   describe '#sport' do
-    event = Event.new(date: Date.tomorrow,
-                      start_time: '20:00:00', end_time: '21:00:00',
-                      university_location: 'Imperial College London')
-
     it 'should fail sport validation' do
-      event.sport = 'Cooking'
-      event.valid? # run validations
+      event = build(:event, sport: 'Cooking')
+      expect(event).to_not be_valid
       expect(event).to have(1).errors_on(:sport)
-      expect(event.errors_on(:sport)).to include('Sport choice is invalid.')
     end
 
     it 'should pass sport validation' do
-      event.sport = 'Football'
-      event.valid? # run validations
-      expect(event).to have(0).errors_on(:sport)
+      event = build(:event)
+      expect(event).to be_valid
     end
   end
 
