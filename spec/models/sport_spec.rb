5f43773417d0f5a1192d95f3938dae9e621149c8
fre14 <femile@blueyonder.co.uk>
2016-06-05 19:08:10 +0100

    Create test that checks if number sport database entries matches number of files in images/sports

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Sport, type: :model do
-  
+
+  describe '#sport database table' do
+    it 'should have same # of entries as # of files in images/sports' do
+      number_entries = Sport.distinct.count('name')
+      number_sport_logos = Dir['app/assets/images/sports/*.png'].length
+      expect(number_entries).to eq(number_sport_logos)
+    end
+  end
+
 end
