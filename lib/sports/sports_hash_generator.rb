9e89c0b912ec5859003ece03ee49605efa13246f
fre14 <femile@blueyonder.co.uk>
2016-06-05 18:33:05 +0100

    Create new script for generating sport_table hash - does not need txt file

--- lib/sports/sports_hash_generator.rb
+++ lib/sports/sports_hash_generator.rb
@@ -3,16 +3,23 @@ module SportsHashGenerator
 
   def self.generate_sports_hash
     sports_table = Hash.new
-    sports_list_path = 'lib/sports/sports_all.txt'
+    sports_list_path = 'app/assets/images/sports/*.png'
 
-    IO.foreach(sports_list_path) do |line|
-      ## Removing space after sport name
-      sport = line.chomp
-      if !sports_table.has_key?(sport)
-        sports_table[sport] = ActionController::Base.helpers.path_to_image("#{sport.downcase}.png")
-      end
+    Dir.glob(sports_list_path) do |sport_logo|
+      sport = File.basename(sport_logo, ".png").titleize
+      sports_table[sport] =
+        ActionController::Base.helpers.path_to_image("#{sport_logo}")
     end
-    
+
+
+    # IO.foreach(sports_list_path) do |line|
+    #   ## Removing space after sport name
+    #   sport = line.chomp
+    #   if !sports_table.has_key?(sport)
+    #     sports_table[sport] = ActionController::Base.helpers.path_to_image("#{sport.downcase}.png")
+    #   end
+    # end
+
     sports_table
   end
 
