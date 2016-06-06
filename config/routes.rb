90235fd3b672d18c020f8efe86576e11a3d9b70a
Paul Vidal <paul.vidal14@imperial.ac.uk>
2016-06-06 17:49:10 +0100

    commented routes file

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,16 @@ Rails.application.routes.draw do
   # Feed is main page
   root 'feed#index'
 
-  # http request redirected to create function in events_controller
+  # http request redirected to events_controller
   resources :events, only: [:index, :create]
 
-  # http request redirected to create function in university_mails_controller
+  # http request redirected to university_mails_controller
   resources :university_mails, only: [:index]
 
-  # http request redirected to create function in event_participants_controller
+  # http request redirected to event_participants_controller
   resources :event_participants, only: [:create]
 
-  # http requested redirected to create function in profile_controller
+  # http requested redirected to profile_controller
   resources :profile, only: [:index]
   get 'profile/created_events', to: 'profile#get_created_events'
   get 'profile/joined_events', to: 'profile#get_joined_events'
