2d6782cf0003924f4872559de32742bf5eff2858
fre14 <femile@blueyonder.co.uk>
2016-06-06 11:15:17 +0100

    Remove unecessary titlisation

--- lib/sports/sports_hash_generator.rb
+++ lib/sports/sports_hash_generator.rb
@@ -1,12 +1,11 @@
 module SportsHashGenerator
-  include ActionView::Helpers::AssetUrlHelper, ActionView::Helpers::AssetTagHelper
 
   def self.generate_sports_hash
     sports_table = Hash.new
     sports_list_path = 'app/assets/images/sports/*.png'
 
     Dir.glob(sports_list_path) do |sport_logo|
-      sport = File.basename(sport_logo, ".png").titleize
+      sport = File.basename(sport_logo, ".png")
       sports_table[sport.titleize] =
         ActionController::Base.helpers.path_to_image("#{sport.downcase}.png")
     end
